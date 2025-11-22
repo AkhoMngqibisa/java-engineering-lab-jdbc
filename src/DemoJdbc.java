a92f93741b86f423b5b55791e50b96c71e0999f2
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-22 20:00:00 +0200

    feat(jdbc-connection): create a connection

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -7,6 +7,12 @@ public class DemoJdbc {
             // Load and register
             Class.forName("org.postgresql.Driver");
 
+            // Create a connection
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "postgres";
+            String password = "password";
+            Connection connection = DriverManager.getConnection(url,user,password);
+
         } catch(Exception e){
             System.err.println(e.getMessage());
         }
