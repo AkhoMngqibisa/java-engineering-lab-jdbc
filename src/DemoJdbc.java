c5dc0466ed1d6b012090da4d650c43656d28031c
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-22 19:45:00 +0200

    feat(jdbc-connection): load and register the driver

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -3,5 +3,12 @@ import java.sql.*;
 public class DemoJdbc {
     public static void main(String[] args) {
 
+        try {
+            // Load and register
+            Class.forName("org.postgresql.Driver");
+
+        } catch(Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 }
