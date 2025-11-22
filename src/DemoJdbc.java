85983ba10c09ee916caafaa3450718deba799c65
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-22 20:15:00 +0200

    feat(jdbc-connection): create a statement

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -11,7 +11,14 @@ public class DemoJdbc {
             String url = "jdbc:postgresql://localhost:5432/postgres";
             String user = "postgres";
             String password = "password";
+            String query = "select * from student where studentNumber = 123";
+
             Connection connection = DriverManager.getConnection(url,user,password);
+            System.out.println("Connected to database successfully");
+
+            Statement statement = connection.createStatement();
+            statement.executeQuery(query);
+
 
         } catch(Exception e){
             System.err.println(e.getMessage());
